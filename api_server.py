f33a49722f916cb7a39b55633c43fc1da17fd79a
Carlos Caro <carlosjesuscaro@icloud.com>
2018-03-04 11:55:22 -0500

    geocode

--- api_server.py
+++ api_server.py
@@ -1,5 +1,6 @@
 #THIS IS A WEBSERVER FOR DEMONSTRATING THE TYPES OF RESPONSES WE SEE FROM AN API ENDPOINT
 from flask import Flask
+import json, requests
 app = Flask(__name__)
 
 #GET REQUEST
@@ -21,6 +22,24 @@ def updateRequestHello():
 @app.route('/deleteHello', methods = ['DELETE'])
 def deleteRequestHello():
 	return "Deleting your hard drive.....haha just kidding! I received a DELETE request!"
+
+@app.route('/foursquare', methods = ['GET'])
+def foursquare():
+	url = 'https://api.foursquare.com/v2/venues/explore'
+
+	params = dict(
+	  client_id='542SEDG5TXXHOPT1IITPXGMTT3AVPC3YEKIZKRO44ZXCQCRI',
+	  client_secret='TTFJODOXG4YWM1AOGMJLURBHNUFVCAMDXTYJ4K1J2TAYYBRO',
+	  v='20170801',
+	  ll='40.7243,-74.0018',
+	  query='coffee',
+	  limit=1
+	)
+
+	resp = requests.get(url=url, params=params)
+	data = json.loads(resp.text)
+	print(data)
+	return data
 
 if __name__ == '__main__':
     app.debug = True
