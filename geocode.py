c913387a89d469319ef3db0bf11e90bb80578d3f
Carlos Caro <carlosjesuscaro@icloud.com>
2018-03-04 12:04:55 -0500

    geocode latitude and longitude

--- geocode.py
+++ geocode.py
@@ -10,4 +10,6 @@ def getGeocodeLocation(inputString):
 	response, content = h.request(url, 'GET')
 	result = json.loads(content)
 	print "response header: %s \n \n"%response
-	return result
+	latitude = result['results'][0]['geometry']['location']['lat']
+	longitude = result['results'][0]['geometry']['location']['lng']
+	return (latitude, longitude)
