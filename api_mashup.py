6aa6f9d5044521917bbf3e0a7acb6ab52b6e097e
Carlos Caro <carlosjesuscaro@icloud.com>
2018-03-04 16:38:48 -0500

    API Mashup Complete

--- api_mashup.py
+++ api_mashup.py
@@ -14,19 +14,34 @@ foursquare_client_secret = "TTFJODOXG4YWM1AOGMJLURBHNUFVCAMDXTYJ4K1J2TAYYBRO"
 def findARestaurant(mealType,location):
 	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
 	latitude, longitude = getGeocodeLocation(location)
-	print latitude
-	print longitude
-
-	return
 	
 	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
-	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
-
+	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?
+	#client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+	url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s'%(foursquare_client_id,foursquare_client_secret,latitude,longitude,mealType))
+	h = httplib2.Http()
+	response, content = h.request(url, 'GET')
+	result = json.loads(content)
 	#3. Grab the first restaurant
+	venue_id = result['response']['venues'][0]['id']
+	venue_name = result['response']['venues'][0]['name']
+	venue_address = result['response']['venues'][0]['location']
 	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
+	url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&client_secret=%s&v=20130815'%(venue_id,foursquare_client_id,foursquare_client_secret))
+	h = httplib2.Http()
+	response, content = h.request(url, 'GET')
+	result = json.loads(content)
 	#5. Grab the first image
+	if result['response']['photos']['items']:
+		image_url = result['response']['photos']['items'][0]['source']['url']
+	else:
+		image_url = 'ALIANZA LIMA'
+
 	#6. If no image is available, insert default a image url
 	#7. Return a dictionary containing the restaurant name, address, and image url	
+	outcome = {'name':venue_name, 'address':venue_address, 'image':image_url}
+	return outcome
+
 if __name__ == '__main__':
 	findARestaurant("Pizza", "Tokyo, Japan")
 	findARestaurant("Tacos", "Jakarta, Indonesia")
